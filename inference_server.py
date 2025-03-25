531aa09c9165fd47885a994716d676f822087976
Juan Carlos Roldan <jaceroldan16@gmail.com>
2025-03-25 13:25:00 +0800

    Add initial agent tool call usage with Hermes-LLaMA

--- inference_server.py
+++ inference_server.py
@@ -1,3 +1,68 @@
+def transform_agent(translate_x: float, translate_y: float, translate_z: float, degrees_x: float, degrees_y: float, degrees_z: float):
+    """
+    Transforms the agent by the specified translation and rotation with respect to the environment.
+    
+    Args:
+        translate_x: Move the agent translate_x units to the right if this is positive, or to the left if negative.
+        translate_y: Move the agent translate_y units up if this is positive, or down if negative.
+        translate_z: Move the agent translate_z units forward if this is positive, or backward if negative.
+        degrees_x: Rotate the agent's camera degrees_x units down if this is positive, or to up if negative.
+        degrees_y: Rotate the agent's camera degrees_y units to the right if this is positive, or to the left if negative.
+        degrees_z: Rotate agent's camera degrees_z units counterclockwise on its axis if this is positive, or clockwise if negative.
+    Returns:
+        The new position of the agent (x, y, z) with respect to the environment and its current camera rotation (x, y, z).
+    """
+    return 1
+
+def transform_hands(
+        left_translate_x: float, left_translate_y: float, left_translate_z: float,
+        left_degrees_x: float, left_degrees_y: float, left_degrees_z: float,
+        right_translate_x: float, right_translate_y: float, right_translate_z: float,
+        right_degrees_x: float, right_degrees_y: float, right_degrees_z: float):
+    """
+    Transforms the agent's left hand and right hand by the specified translation and rotation with respect to the environment.
+    
+    Args:
+        left_translate_x: Move the agent's left hand left_translate_x units to the right if this is positive, or to the left if negative.
+        left_translate_y: Move the agent's left hand left_translate_y units up if this is positive, or down if negative.
+        left_translate_z: Move the agent's left hand left_translate_z units forward if this is positive, or backward if negative.
+        left_degrees_x: Rotate the agent's left hand rotation left_degrees_x units down if this is positive, or up if negative.
+        left_degrees_y: Rotate the agent's left hand rotation left_degrees_y units to the right if this is positive, or to the left if negative.
+        left_degrees_z: Rotate the agent's left hand rotation left_degrees_z units counterclockwise on its axis if this is positive, or clockwise if negative.
+        right_translate_x: Move the agent's right hand right_translate_x units to the right if this is positive, or to the left if negative.
+        right_translate_y: Move the agent's right hand right_translate_y units up if this is positive, or down if negative.
+        right_translate_z: Move the agent's right hand right_translate_z units forward if this is positive, or backward if negative.
+        right_degrees_x: Rotate the agent's right hand rotation right_degrees_x units down if this is positive, or up if negative.
+        right_degrees_y: Rotate the agent's right hand rotation right_degrees_y units to the right if this is positive, or to the left if negative.
+        right_degrees_z: Rotate the agent's right hand rotation right_degrees_z units counterclockwise on its axis if this is positive, or clockwise if negative.
+    Returns:
+        The new position of the agent's hands (x, y, z for both hands) with respect to the environment and both hands' current camera rotation (x, y, z for both).
+    """
+    return 1
+
+def toggle_left_grip():
+    """
+    Toggles the grip of the left hand. Will successfully toggle from false to true if an XR Grab Interactable object collides with the left hand.
+
+    Returns:
+        True if an object has been grabbed and False, otherwise.
+    """
+    return 1
+
+
+def toggle_right_grip():
+    """
+    Toggles the grip of the left hand. Will successfully toggle from false to true if an XR Grab Interactable object collides with the left hand.
+
+    Returns:
+        True if an object has been grabbed and False, otherwise.
+    """
+    return 1
+
+
+tools = [transform_agent, transform_hands, toggle_left_grip, toggle_right_grip]
+
+
 from fastapi import FastAPI, File, UploadFile, HTTPException, Form
 from PIL import Image
 from io import BytesIO
@@ -5,6 +70,7 @@ from io import BytesIO
 
 # SmolVLM - Instruct
 from transformers import AutoProcessor, AutoModelForVision2Seq
+from transformers import AutoTokenizer, LlamaForCausalLM
 # Qwen usage
 from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info  # Make sure this is available
@@ -17,21 +83,22 @@ import numpy as np
 SMOLVLM_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 processor = AutoProcessor.from_pretrained("HuggingFaceTB/SmolVLM-Instruct")
-model = AutoModelForVision2Seq.from_pretrained("HuggingFaceTB/SmolVLM-Instruct",
+smol_vlm_model = AutoModelForVision2Seq.from_pretrained("HuggingFaceTB/SmolVLM-Instruct",
                                                torch_dtype=torch.bfloat16,
                                                _attn_implementation="flash_attention_2" if SMOLVLM_DEVICE == "cuda" else "eager").to(SMOLVLM_DEVICE)
-model.to(SMOLVLM_DEVICE)
+smol_vlm_model.to(SMOLVLM_DEVICE)
 
-QWEN_DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-model = Qwen2VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2-VL-2B-Instruct",
-    torch_dtype=torch.bfloat16,
-    attn_implementation="flash_attention_2",
-    device_map={"" : 0}  # Force model to GPU
+tokenizer = AutoTokenizer.from_pretrained('NousResearch/Hermes-2-Pro-Llama-3-8B', trust_remote_code=True)
+
+HERMES_DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
+hermes_model = LlamaForCausalLM.from_pretrained(
+    "NousResearch/Hermes-2-Pro-Llama-3-8B",
+    torch_dtype=torch.float16,
+    device_map="auto",
+    load_in_8bit=False,
+    load_in_4bit=True,
+    use_flash_attention_2=True
 )
-
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
-
 
 # processing image
 app = FastAPI()
@@ -57,7 +124,7 @@ async def caption_image(prompt: str = Form(...), file: UploadFile = File(...)):
     inputs = processor(text=prompt, images=[frame], return_tensors="pt")
     inputs = inputs.to(SMOLVLM_DEVICE)
 
-    generated_ids = model.generate(**inputs, max_new_tokens=256)
+    generated_ids = smol_vlm_model.generate(**inputs, max_new_tokens=256)
     generated_texts = processor.batch_decode(
         generated_ids,
         skip_special_tokens=True,
@@ -87,28 +154,17 @@ async def predict(prompt: str = Form(...), file: UploadFile = File(...)):
             }
         ]
 
-        # Process inputs
-        text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
-        image_inputs, video_inputs = process_vision_info(messages)
+        messages = [
+            {"role": "user", "content": prompt}
+        ]
 
-        inputs = processor(
-            text=[text],
-            images=image_inputs,
-            videos=video_inputs,
-            padding=True,
-            return_tensors="pt",
-        )
+        inputs = tokenizer.apply_chat_template(messages, chat_template="tool_use", tools=tools, add_generation_prompt=True, return_dict=True, return_tensors="pt")
+        inputs = {k: v.to(hermes_model.device) for k, v in inputs.items()}
+        out = hermes_model.generate(**inputs, max_new_tokens=256)
+        response = tokenizer.decode(out[0][len(inputs["input_ids"][0]):])
+        print(response)
 
-        # Move inputs to the model's device
-        inputs = {k: v.to(QWEN_DEVICE) for k, v in inputs.items()}
-
-        # Generate response
-        generated_ids = model.generate(**inputs, max_new_tokens=128)
-        generated_text = processor.batch_decode(
-            generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False
-        )
-
-        return {"response": generated_text}
+        return {"response": response}
 
     except Exception as e:
         import traceback; traceback.print_exc()
